dd53bf886abc07ddab2b831f5fb893ede2a58634
ixAbdulaziz <AzzMoh011@gmail.com>
2025-07-10 14:48:52 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,55 @@
+// models/User.js
+import { DataTypes } from 'sequelize';
+import sequelize from '../database/connection.js';
 
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true,
+      len: [3, 50]
+    }
+  },
+  passwordHash: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    field: 'password_hash'
+  },
+  role: {
+    type: DataTypes.ENUM('user', 'admin'),
+    defaultValue: 'user',
+    allowNull: false
+  },
+  lastLogin: {
+    type: DataTypes.DATE,
+    field: 'last_login'
+  },
+  loginCount: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+    field: 'login_count'
+  }
+}, {
+  tableName: 'users',
+  indexes: [
+    {
+      unique: true,
+      fields: ['username']
+    },
+    {
+      fields: ['role']
+    },
+    {
+      fields: ['last_login']
+    }
+  ]
+});
+
+export default User;
